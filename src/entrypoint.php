8189b4b4cb15d9709729899c5ac23642904a398d
Dave Gaeddert <dave.gaeddert@gmail.com>
2018-01-05 13:34:02 -0600

    use github token in composer install

--- src/entrypoint.php
+++ src/entrypoint.php
@@ -5,6 +5,13 @@ require __DIR__ . '/vendor/autoload.php';
 require_once 'collect.php';
 require_once 'utils.php';
 
+// make any GitHub repos installable that this token also has access to
+$github_token = getenv('GITHUB_API_TOKEN');
+if ($github_token) {
+    shell_exec("echo -e \"machine github.com\n  login x-access-token\n  password $github_token\" >> ~/.netrc");
+    shell_exec("composer config -g http-basic.github.com x-access-token $github_token");
+}
+
 if (getenv('RUN_AS') === 'collector') {
     echo "Running as collector\n";
 
